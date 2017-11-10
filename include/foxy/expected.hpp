6ced04df89ae7eb335245ed87f614acb6915c820
LeonineKing1199 <christian.mazakas@gmail.com>
2017-11-09 18:19:20 -0800

    initial commit

---
+++ include/foxy/expected.hpp
@@ -0,0 +1,100 @@
+#ifndef FOXY_EXPECTED_HPP_
+#define FOXY_EXPECTED_HPP_
+
+#include <type_traits>
+#include <boost/variant/get.hpp>
+#include <boost/variant/variant.hpp>
+
+namespace foxy
+{
+  struct fmap;
+
+  template <
+    typename T, typename E,
+    typename = std::enable_if_t<!std::is_same_v<T, E>>
+  >
+  struct expected
+  {
+  private:
+    boost::variant<T, E> data_;
+
+    friend fmap;
+
+  public:
+    template <
+      typename = std::enable_if_t<std::is_default_constructible_v<T>>,
+      typename = std::enable_if_t<std::is_default_constructible_v<E>>
+    >
+    expected(void)
+    : data_{}
+    {}
+
+    template <
+      typename = std::enable_if_t<std::is_copy_constructible_v<T>>,
+      typename = std::enable_if_t<std::is_copy_constructible_v<E>>
+    >
+    expected(expected const& rhs)
+    : data_{rhs.data_}
+    {}
+
+    template <
+      typename = std::enable_if_t<std::is_move_constructible_v<T>>,
+      typename = std::enable_if_t<std::is_move_constructible_v<E>>
+    >
+    expected(expected&& rhs)
+    : data_{std::move(rhs).data_}
+    {}
+
+    template <
+      typename = std::enable_if_t<std::is_copy_constructible_v<T>>
+    >
+    expected(T const& t)
+    : data_{t}
+    {}
+
+    template <
+      typename = std::enable_if_t<std::is_move_constructible_v<T>>
+    >
+    expected(T&& t)
+    : data_{std::move(t)}
+    {}
+
+    template <
+      typename = std::enable_if_t<std::is_copy_constructible_v<E>>
+    >
+    expected(E const& e)
+    : data_{e}
+    {}
+
+    template <
+      typename = std::enable_if_t<std::is_move_constructible_v<E>>
+    >
+    expected(E&& e)
+    : data_{std::move(e)}
+    {}
+
+    auto is_valid(void) const noexcept -> bool
+    {
+      return data_.which() == 0;
+    }
+  };
+
+  struct fmap
+  {
+    template <typename F, typename T, typename E>
+    auto operator()(F&& f, expected<T, E> const& e) const
+      -> expected<decltype(std::forward<F>(f)(std::declval<T>())), E>
+    {
+      using boost::get;
+      using result_type = expected<decltype(std::forward<F>(f)(std::declval<T>())), E>;
+
+      if (e.data_.which() == 0) {
+        return {std::forward<F>(f)(get<T>(e.data_))};
+      } else {
+        return {get<E>(e.data_)};
+      }
+    }
+  };
+}
+
+#endif // FOXY_EXPECTED_HPP_
---
+++ tests/expected_test.cpp
@@ -0,0 +1,69 @@
+#include <vector>
+#include <iostream>
+#include <exception>
+#include <fit/infix.hpp>
+
+#include "foxy/expected.hpp"
+
+#include <catch.hpp>
+
+TEST_CASE("Our Expected type")
+{
+  using expected_t = foxy::expected<int, std::exception>;
+
+  SECTION("should be default-constructible for a given type")
+  {
+    expected_t expected{};
+    REQUIRE(expected.is_valid());
+  }
+
+  SECTION("should be copy-constructible")
+  {
+    auto const expected  = expected_t{};
+    auto const expected2 = expected_t{expected};
+    REQUIRE(expected.is_valid());
+    REQUIRE(expected2.is_valid());
+  }
+
+  SECTION("should be parameter-constructible")
+  {
+    auto const expected = expected_t{1337};
+    REQUIRE(expected.is_valid());
+  }
+
+  SECTION("should should support moving parameters in its constructor")
+  {
+    auto v = std::vector<int>{1, 2, 3, 4};
+
+    auto const expected =
+      foxy::expected<
+        std::vector<int>,
+        std::exception
+      >{std::move(v)};
+
+    REQUIRE(expected.is_valid());
+  }
+
+  SECTION("should support fmap to some degree")
+  {
+    auto expected = expected_t{1337};
+
+    auto const fmap = foxy::fmap{};
+
+    expected = fmap(
+      [](int const x) -> int { return x + 1; },
+      expected);
+
+    auto const f = [](int const x) -> int
+    {
+      std::cout << x << "\n";
+      return x;
+    };
+
+    auto const $ = fit::infix(fmap);
+
+    expected = (f <$> expected);
+
+    REQUIRE(expected.is_valid());
+  }
+}
